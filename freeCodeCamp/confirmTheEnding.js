7dc1f9aebfb6cf168c726819ea3bfbe24a718bd5
Elva K Chung <chrihoon@gmail.com>
2020-03-24 00:33:59 -0400

    update6_Confirm The Ending

---
+++ freeCodeCamp/confirmTheEnding.js
@@ -0,0 +1,19 @@
+// Confirm the ending
+function confirmEnding1(str, target){
+
+    return (str.substr(-target.length) === target) ;
+}
+
+console.log(confirmEnding1("Bastian", "n"));
+
+function confirmEnding2(str, target){
+    return str.endsWith(target);
+}
+
+console.log(confirmEnding3("Bastian", "a"));
+
+function confirmEnding(str, target){
+    return str.slice(str.length - target.length) === target;    
+}
+
+console.log(confirmEnding3("Bastian", "p"));
